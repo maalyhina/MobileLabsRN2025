0b7c121deee990fbadab75c61256294712cb98f4
Малихіна Ангеліна Олегівна <vt231_mao@student.ztu.edu.ua>
2025-05-29 17:03:05 +0300

    feat: Add navigation between Home and Tasks screens using React Navigation

--- ThirdMobileApp/src/navigation/Navigator.js
+++ ThirdMobileApp/src/navigation/Navigator.js
@@ -6,13 +6,13 @@ import TasksScreen from '../src/screens/TasksScreen';
 
 const Stack = createNativeStackNavigator();
 
-export default function Navigator() {
-	return (
-		<NavigationContainer>
-			<Stack.Navigator initialRouteName="Home">
-				<Stack.Screen name="Home" component={HomeScreen} />
-				<Stack.Screen name="Tasks" component={TasksScreen} />
-			</Stack.Navigator>
-		</NavigationContainer>
-	);
+export default function AppNavigator() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Home">
+                <Stack.Screen name="Home" component={HomeScreen} />
+                <Stack.Screen name="Tasks" component={TasksScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
