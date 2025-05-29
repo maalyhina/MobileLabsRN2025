243c6f87521b9dbf6dc938691d616d65031bebad
Малихіна Ангеліна Олегівна <vt231_mao@student.ztu.edu.ua>
2025-05-29 16:14:05 +0300

    feat: add navigation stack with HomeScreen and TasksScreen

---
+++ ThirdMobileApp/src/navigation/Navigator.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from '../src/screens/HomeScreen';
+import TasksScreen from '../src/screens/TasksScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function Navigator() {
+	return (
+		<NavigationContainer>
+			<Stack.Navigator initialRouteName="Home">
+				<Stack.Screen name="Home" component={HomeScreen} />
+				<Stack.Screen name="Tasks" component={TasksScreen} />
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
+}
