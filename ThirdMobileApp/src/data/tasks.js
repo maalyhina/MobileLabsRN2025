b8a554eef85a8179409c366b93c5a782d69f7f84
Малихіна Ангеліна Олегівна <vt231_mao@student.ztu.edu.ua>
2025-05-29 17:23:36 +0300

    feat: Add initial task list data for task completion tracking

---
+++ ThirdMobileApp/src/data/tasks.js
@@ -0,0 +1,10 @@
+export default [
+    { id: 1, title: "Зробити 10 кліків", completed: false },
+    { id: 2, title: "Зробити 5 подвійних кліків", completed: false },
+    { id: 3, title: "Утримувати 3 секунди", completed: false },
+    { id: 4, title: "Перетягнути об'єкт", completed: false },
+    { id: 5, title: "Свайп вправо", completed: false },
+    { id: 6, title: "Свайп вліво", completed: false },
+    { id: 7, title: "Масштабувати об'єкт", completed: false },
+    { id: 8, title: "Отримати 100 очок", completed: false },
+];
